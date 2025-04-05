fcf66df20e87edb58a4c5575e28705b6379452a5
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-04-05 00:25:44 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,47 @@
+
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+	public static void selectionSortName(String[] name, int[] ages){
+		for (int i=0; i<name.length-1;i++){
+			int smallest=i;
+			for (int j=i+1; j<name.length;j++){
+				if(name[j].toLowerCase().compareTo(name[smallest].toLowerCase()) < 0){
+					smallest=j;
 
-	
+				}
+			}
+				String temp=name[smallest];
+					name[smallest]=name[i];
+					name[i]=temp;
+					int tempr=ages[smallest];
+					ages[smallest]=ages[i];
+					ages[i]=tempr;
+			
+		}
+
+	}
+	public static void selectionSortAge(String[] name, int[] ages){
+		int smallest=0;
+		for (int i=0; i<ages.length-1;i++){
+			for (int j=i+1; j<ages.length;j++){
+				if(ages[smallest]>ages[j]){
+					smallest=j;
+				}
+			}
+				String temp=name[smallest];
+				name[smallest]=name[i];
+				name[i]=temp;
+				int tempr=ages[smallest];
+				ages[smallest]=ages[i];
+				ages[i]=tempr;
+
+		}
+
+	}
 
 }
